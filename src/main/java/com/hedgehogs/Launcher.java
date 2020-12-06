f01cb6229d21d393cc74cc05986d71fde50f87cc
nataliaezh <natalya_ezhkova@mail.ru>
2020-12-06 14:09:50 +0300

    Изменена реализация настроек приложения с xml-based на annotation-based, добавлен application.properties

--- src/main/java/com/hedgehogs/Launcher.java
+++ src/main/java/com/hedgehogs/Launcher.java
@@ -2,9 +2,12 @@ package com.hedgehogs;
 
 import com.hedgehogs.entities.Glasses;
 import com.hedgehogs.entities.Lenses;
+import com.hedgehogs.repositories.LensesRepository;
+import com.hedgehogs.services.LensesService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,9 +16,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Launcher {
 	private static final Logger log  = LogManager.getLogger(Launcher.class.getName());
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		Glasses glasses = context.getBean("glasses", Glasses.class);
 		Lenses lenses  = context.getBean("lenses", Lenses.class);
+		LensesRepository lensesRepository = context.getBean("lensesRepository", LensesRepository.class);
+		LensesService lensesService = context.getBean("lensesService", LensesService.class);
 			lenses.setId(1L);
 			lenses.setDiopters("+1");
 			lenses.setSize("5sm");
@@ -23,8 +28,5 @@ public class Launcher {
 		System.out.println(lenses);
 		System.out.println("Сохранение заказа на изготовление со следующими параметрами: " + glasses.getLensesService().save(lenses).toString());
 		glasses.doGlasses();
-
-
-
 	}
 }
